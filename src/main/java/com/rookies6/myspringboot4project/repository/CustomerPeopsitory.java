2e185f5063cc73be2cb373c9c8dd437bb6df3b2e
jero-06 <jeeyoung345@gmail.com>
2026-09-01 15:57:22 +0900

    CustomerRepository add

---
+++ src/main/java/com/rookies6/myspringboot4project/repository/CustomerPeopsitory.java
@@ -0,0 +1,11 @@
+package com.rookies6.myspringboot4project.repository;
+
+import com.rookies6.myspringboot4project.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository {
+import com.rookies6.myspringboot4project.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+    public interface CustomerRepository extends JpaRepository<Customer,Long> {
+    }
