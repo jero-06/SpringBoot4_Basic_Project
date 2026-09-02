d867f45bbb3b859260f2fe6e42b9a98a97dff11b
jero-06 <jeeyoung345@gmail.com>
2026-09-02 16:45:32 +0900

    DefaultExceptionAdvice add

--- src/main/java/com/rookies6/myspringboot4project/exception/advice/ErrorObject.java
+++ src/main/java/com/rookies6/myspringboot4project/exception/advice/ErrorObject.java
@@ -7,8 +7,11 @@ import java.util.Locale;
 
 @Data
 public class ErrorObject {
+    // 에러 코드
     private Integer statusCode;
+    // 에러 메세지
     private String message;
+    // 에러 발생 시간
     private String timestamp;
 
     public String getTimestamp() {
