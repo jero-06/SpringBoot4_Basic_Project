6ecfa1c587d24718e2d28160d8da2b9173a9ba22
jero-06 <jeeyoung345@gmail.com>
2026-09-02 15:35:35 +0900

    User RestController update

--- src/main/java/com/rookies6/myspringboot4project/controller/UserRestController.java
+++ src/main/java/com/rookies6/myspringboot4project/controller/UserRestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
-@RequestMapping("\api\users")
+@RequestMapping("/api/users")
 public class UserRestController {
     private UserRepository userRepository;
 
