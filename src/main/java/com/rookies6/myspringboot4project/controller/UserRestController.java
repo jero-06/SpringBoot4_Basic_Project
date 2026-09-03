6961c05f048cdeb442431a459433e5273f3d553d
jero-06 <jeeyoung345@gmail.com>
2026-09-03 11:02:14 +0900

    WebConfig add

--- src/main/java/com/rookies6/myspringboot4project/controller/UserRestController.java
+++ src/main/java/com/rookies6/myspringboot4project/controller/UserRestController.java
@@ -67,7 +67,7 @@ public class UserRestController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteUser(@PathVariable Long id) {
-        User existUser = getUser(userRepository.findById());
+        User existUser = getUser(userRepository.findById(id));
         userRepository.delete(existUser);
         return ResponseEntity.ok("Id = " + id + " User가 삭제 되었습니다.");
     }
