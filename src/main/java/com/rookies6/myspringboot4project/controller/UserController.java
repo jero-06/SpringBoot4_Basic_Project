1b8e6c8ba3334c82ee50ea91857bfab0885e7b12
jero-06 <jeeyoung345@gmail.com>
2026-09-03 15:32:08 +0900

    User update

--- src/main/java/com/rookies6/myspringboot4project/controller/UserController.java
+++ src/main/java/com/rookies6/myspringboot4project/controller/UserController.java
@@ -2,11 +2,14 @@ package com.rookies6.myspringboot4project.controller;
 
 import com.rookies6.myspringboot4project.entity.User;
 import com.rookies6.myspringboot4project.repository.UserRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -33,6 +36,19 @@ public class UserController {
     @GetMapping("/signup")
     public String showSignUpForm(@ModelAttribute("userForm") User user) {
         return "add-user";
+    }
+
+    //입력항목을 검증하고 등록처리를 하는 메서드
+    @PostMapping("/adduser")
+    public String addUser(@Valid @ModelAttribute("userForm") User user,
+                          BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "add-user";
+        }
+        userRepository.save(user);
+        model.addAttribute("users", userRepository.findAll());
+        return "index";
+//        return "redirect:/index";
     }
 
 
