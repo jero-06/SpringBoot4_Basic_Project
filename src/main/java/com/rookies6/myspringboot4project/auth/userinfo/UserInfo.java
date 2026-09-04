9c528e16099eca98685063857b468ac61f1984e4
jero-06 <jeeyoung345@gmail.com>
2026-09-04 13:41:28 +0900

    userinfo add

---
+++ src/main/java/com/rookies6/myspringboot4project/auth/userinfo/UserInfo.java
@@ -0,0 +1,30 @@
+package com.rookies6.myspringboot4project.auth.userinfo;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String roles;
+}
