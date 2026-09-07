973991c64d5ea977a09f65da1fc86f6c2d414ab8
jero-06 <jeeyoung345@gmail.com>
2026-09-07 15:35:18 +0900

    StudentRepository update

--- src/main/java/com/rookies6/myspringboot4project/runner/DataInitRunner.java
+++ src/main/java/com/rookies6/myspringboot4project/runner/DataInitRunner.java
@@ -108,7 +108,7 @@ public class DataInitRunner implements CommandLineRunner {
         Student student = Student.builder()
                 .name(name)
                 .studentNumber(studentNumber)
-                //양방향 연관관계 설정
+                //양방향 연관관계 설정 Student 객체와 StudentDetail 객체를 연결
                 .studentDetail(detail)
                 .build();
 
